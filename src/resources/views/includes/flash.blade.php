5643a77910c5cd08534c20dbcd2fced108e06a35
Daniel Bennin <danyelamps.db@gmail.com>
2018-07-24 06:58:11 +0000

    Error Handling

--- src/resources/views/includes/flash.blade.php
+++ src/resources/views/includes/flash.blade.php
@@ -5,4 +5,15 @@
         </button>
         {{ session('status') }}
     </div>
+@elseif($errors->any())
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <ul>
+            @foreach($errors->all() as $err)
+                <li>{{ $err  }}</li>
+            @endforeach
+        </ul>
+    </div>
 @endif
